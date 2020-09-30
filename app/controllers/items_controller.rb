5ab226d6dc8c390993ce8bd94603e465d58d0a03
mmtt875 <875morihana@gmail.com>
2020-09-30 10:20:11 +0900

    ログインしていない場合のサインインページへの遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,5 @@
 class ItemsController < ApplicationController
+  before_action :move_to_sign_in, except:[:new]
 
   def index
   end
@@ -6,4 +7,11 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
+
+  private
+  def move_to_sign_in
+    unless user_signed_in?
+      redirect_to '/users/sign_in'
+      
+    end
 end
