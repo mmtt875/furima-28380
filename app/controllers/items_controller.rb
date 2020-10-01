fa6ba1f963cbdd2f17dd73a09c1d48e19edf127d
mmtt875 <875morihana@gmail.com>
2020-10-01 16:37:02 +0900

    商品を出品してデータベースに保存

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,10 +8,25 @@ class ItemsController < ApplicationController
     @item = Item.new
   end
 
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+       redirect_to root_path
+    else
+       render new_item_path
+    end
+  end
+
   private
   def move_to_sign_in
     unless user_signed_in?
       redirect_to '/users/sign_in'
     end
+  end
+  
+  def item_params
+    params.require(:item).permit(
+    :name, :image, :description, :category_id, :status_id, :delivery_fee_id,
+    :area_id,:delivery_days_id, :price).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -6,12 +6,13 @@ class Item < ApplicationRecord
   belongs_to_active_hash :status
   belongs_to_active_hash :delivery_fee
   belongs_to_active_hash :area
-  belongs_to_active_hash :delivery_day
-  validates :image, :category,:status,:delivery_fee, :area,
-            :delivery_day, :price, presence: true
+  belongs_to_active_hash :delivery_days
+  validates :image, :category, :status, :delivery_fee, :area,
+            :delivery_days, :price, presence: true
   validates :name, presence: true, length: { maximum: 40 }
   validates :description, presence: true, length: { maximum: 1000}
+  validates :price, numericality: { only_integer: true, greater_than: 300, less_than: 9999999}
   with_options numericality: { other_than: 1 } do
-    validates :category_id, :status_id, :delivery_fee_id, :area_id, :delivery_day_id
+    validates :category_id, :status_id, :delivery_fee_id, :area_id, :delivery_days_id
   end
 end
