e6f9159f26196eef52e38d93950102da146a40b8
mmtt875 <875morihana@gmail.com>
2020-10-05 17:10:19 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -21,15 +21,15 @@ class ItemsController < ApplicationController
   def show
     @item = Item.find(params[:id])
   end
-  
-  def edit
-    @item = Item.find(params[:id])
-  end
 
-  def update
-    item = Item.find(params[:id])
-    item.update(item_params)
-  end
+  # def edit
+  #   @item = Item.find(params[:id])
+  # end
+
+  # def update
+  #   item = Item.find(params[:id])
+  #   item.update(item_params)
+  # end
 
   def item_params
     params.require(:item).permit(
