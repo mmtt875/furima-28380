79364e03085d852888aeea7f47b83b10a54bbe3d
mmtt875 <875morihana@gmail.com>
2020-10-06 20:01:53 +0900

    購入のモデルとテーブルの作成、READMEの変更

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,8 +10,8 @@ class Item < ApplicationRecord
   validates :image, :category, :status, :delivery_fee, :area,
             :delivery_days, :price, presence: true
   validates :name, presence: true, length: { maximum: 40 }
-  validates :description, presence: true, length: { maximum: 1000}
-  validates :price, numericality: { only_integer: true, greater_than: 300, less_than: 9999999}
+  validates :description, presence: true, length: { maximum: 1000 }
+  validates :price, numericality: { only_integer: true, greater_than: 300, less_than: 9999999 }
   with_options numericality: { other_than: 1 } do
     validates :category_id, :status_id, :delivery_fee_id, :area_id, :delivery_days_id
   end
