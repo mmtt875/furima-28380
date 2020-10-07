ab65cab0e73675681dc00d7cc7b6ff4bdcfb2f85
mmtt875 <875morihana@gmail.com>
2020-10-07 20:57:45 +0900

    コメントアウトしていた部分の解除

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,13 +2,13 @@ class Order
   include ActiveModel::Model
   attr_accessor :user_id, :item_id, :postal_code, :area_id, :city, :address, :building_name, :phone_number, :token
 
-  # validates :postal_code, :area_id, :city,
-  #           :address, :phone_number, :token, presence: true
-  # validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/ }
-  # validates :phone_number, format: { with: /\A[\d]/ }, length:{ maximum: 11 }
-  # with_options numericality: { other_than: 1 } do
-  #   validates :area_id
-  # end
+  validates :postal_code, :area_id, :city,
+            :address, :phone_number, :token, presence: true
+  validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/ }
+  validates :phone_number, format: { with: /\A[\d]/ }, length:{ maximum: 11 }
+  with_options numericality: { other_than: 1 } do
+    validates :area_id
+  end
 
   def save
     purchase = Purchase.create!(user_id: user_id, item_id: item_id)
