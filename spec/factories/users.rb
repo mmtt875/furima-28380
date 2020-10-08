6bdeb2558e5262b20e0c0f359c5a7f711e48f680
mmtt875 <875morihana@gmail.com>
2020-10-08 18:12:06 +0900

    テストコード

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,8 @@ FactoryBot.define do
   factory :user do
     nickname              {Faker::Name.initials}
     email                 {Faker::Internet.free_email}
-    password              {Faker::Internet.password(min_length: 6)}
+    # password              {Faker::Internet.password(min_length: 6)}
+    password              {"aaa123"}
     password_confirmation {password}
     family_name           {"不破"}
     first_name            {"ふわり"}
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -8,20 +8,63 @@ RSpec.describe Order, type: :model do
     item.image = fixture_file_upload("app/assets/images/usagi.jpg")
     item.save
     @order = FactoryBot.build(:order, item_id: item.id, user_id: buyer.id)
+    sleep 1
   end
   describe '商品の購入' do
     context '商品の購入がうまくいくとき' do
       it "全ての項目が正しく入力されていると商品が購入できる" do
+        expect(@order).to be_valid
       end
-      it "" do
+      it "建物名が空でも購入できる" do
+        @order.building_name = ""
+        expect(@order).to be_valid
       end
     end
     context '商品の購入がうまくいかないとき' do
-      it "" do
+      it "郵便番号が空だと購入できない" do
+        @order.postal_code = ""
+        @order.valid?
+        expect(@order.errors.full_messages).to include("Postal code can't be blank")
       end
-      it "" do
+      it "郵便番号にハイフンが入っていないと購入できない" do
+        @order.postal_code = "0000000"
+        @order.valid?
+        expect(@order.errors.full_messages).to include("Postal code is invalid")
       end
-      it "" do
+      it "都道府県が未選択だと購入できない" do
+        @order.area_id = "1"
+        @order.valid?
+        expect(@order.errors.full_messages).to include("Area must be other than 1")
+      end
+      it "市区町村が空だと購入できない" do
+        @order.city = ""
+        @order.valid?
+        expect(@order.errors.full_messages).to include("City can't be blank")
+      end
+      it "番地が空だと購入できない" do
+        @order.address = ""
+        @order.valid?
+        expect(@order.errors.full_messages).to include("Address can't be blank")
+      end
+      it "電話番号が空だと購入できない" do
+        @order.phone_number = ""
+        @order.valid?
+        expect(@order.errors.full_messages).to include("Phone number can't be blank")
+      end
+      it "電話番号が半角数字以外だと購入できない" do
+        @order.phone_number = "おまんじゅう"
+        @order.valid?
+        expect(@order.errors.full_messages).to include("Phone number is invalid")
+      end
+      it "電話番号が11桁以上だと購入できない" do
+        @order.phone_number = "000000111111"
+        @order.valid?
+        expect(@order.errors.full_messages).to include("Phone number is too long (maximum is 11 characters)")
+      end
+      it "トークンがないと購入できない" do
+        @order.token = ""
+        @order.valid?
+        expect(@order.errors.full_messages).to include("Token can't be blank")
       end
     end
   end
